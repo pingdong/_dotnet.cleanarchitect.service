806584b6c567b5aec9debb2cd9aef8b0a395c545
Ping Dong <ping.dong@live.com>
2019-06-02 11:27:15 +1200

    Init Commit

---
+++ src/PingDong.CleanArchitect.Service/Idempotency/RequestManage/RequestManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using PingDong.CleanArchitect.Infrastructure;
+
+namespace PingDong.CleanArchitect.Service.Idempotency
+{
+    internal class RequestManager : IRequestManager
+    {
+        private readonly IRepository<Guid, ClientRequest> _repository;
+
+        public RequestManager(IRepository<Guid, ClientRequest> context)
+        {
+            _repository = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<bool> CheckExistsAsync(Guid id)
+        {
+            var request = await _repository.FindByIdAsync(id);
+
+            return request != null;
+        }
+
+        public async Task CreateRequestRecordAsync<T>(Guid id)
+        { 
+            var exists = await CheckExistsAsync(id);
+
+            var request = exists 
+                            ? throw new RequestDuplicatedException($"Request with {id} already exists") 
+                            : new ClientRequest
+                                {
+                                    Name = typeof(T).Name,
+                                    Time = DateTime.UtcNow
+                                };
+
+            await _repository.AddAsync(request);
+
+            await _repository.UnitOfWork.SaveEntitiesAsync();
+        }
+    }
+}
---
+++ src/PingDong.CleanArchitect.Service/Registrar.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PingDong.CleanArchitect.Service.Idempotency;
+
+namespace PingDong.CleanArchitect.Service
+{
+    public class ServiceRegistrar
+    {
+        public virtual void Register(IServiceCollection services)
+        {
+            // Idempotency: Register RequestManager
+            services.AddScoped(typeof(IRequestManager), typeof(RequestManager));
+        }
+    }
+}
